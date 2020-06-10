570ed1b18a6ef5be5138346cf1a123e7e44fef4a
Veronika <vbessonova@fil-it.ru>
2020-06-10 12:18:10 +0300

    test

--- src/components/AddCard/AddCard.jsx
+++ src/components/AddCard/AddCard.jsx
@@ -17,7 +17,7 @@ const AddCard = ({
   isShowDropdown,
   }) => (
     <>
-      {console.log('test', '7test', '88987', 999999)}
+      {console.log('test', '7test', '88987', 999999, 899)}
       {isShowForm ?
         <form onSubmit={onSubmitValueInInput} className="form" onClick={onClickForm}>
           <input
